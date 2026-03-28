564e64d05d49f9dc69eeac5966c0dcc7c309d4f1
alfu-git <mdalfaz.dev@gmail.com>
2026-03-28 20:12:02 +0600

    create cart empty dashboard

--- src/Components/ModelsContainer/ModelsContainer.jsx
+++ src/Components/ModelsContainer/ModelsContainer.jsx
@@ -16,54 +16,63 @@ const ModelsContainer = ({ modelDataPromise }) => {
     <section className="mb-20 container mx-auto px-5 border-t border-[gray]/10 rounded-t-xl p-2">
       <div>
         <div>
-          {/* tab and title */}
-          <div>
-            {/* tab btn */}
-            <div className="mb-7 sm:mb-15 flex justify-center">
-              <div className="flex">
-                <button
-                  className={`
+          {/* tab btn */}
+          <div className="mb-7 sm:mb-15 flex justify-center">
+            <div className="flex">
+              <button
+                className={`
                   btn w-25 sm:w-50
                   ${tabName === "model" ? "tab-active" : "tab-inactive"}
                 `}
-                  onClick={() => handleTabBtn("model")}
-                >
-                  Models
-                </button>
+                onClick={() => handleTabBtn("model")}
+              >
+                Models
+              </button>
 
-                <button
-                  className={`
+              <button
+                className={`
                   btn w-25 sm:w-50
                   ${tabName === "cart" ? "tab-active" : "tab-inactive"}
                 `}
-                  onClick={() => handleTabBtn("cart")}
-                >
-                  Cart <span>(0)</span>
-                </button>
-              </div>
-            </div>
-
-            {/* title */}
-            <div>
-              <div className="mb-16 text-center">
-                <h2 className="mb-4 text-4xl sm:text-5xl font-bold">
-                  Choose Your AI Model
-                </h2>
-                <p className="text-lg sm:text-xl text-zinc-400">
-                  One subscription gives you access to all frontier AI models
-                </p>
-              </div>
+                onClick={() => handleTabBtn("cart")}
+              >
+                Cart <span>(0)</span>
+              </button>
             </div>
           </div>
 
-          {/* card section */}
+          {/* title and card section */}
           {tabName === "model" && (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-              {modelData.map((model) => (
-                <ModelCard key={model.id} model={model} />
-              ))}
+            <div>
+              {/* title */}
+              <div>
+                <div className="mb-16 text-center">
+                  <h2 className="mb-4 text-4xl sm:text-5xl font-bold">
+                    Choose Your AI Model
+                  </h2>
+                  <p className="text-lg sm:text-xl text-zinc-400">
+                    One subscription gives you access to all frontier AI models
+                  </p>
+                </div>
+              </div>
+
+              {/* card section */}
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                {modelData.map((model) => (
+                  <ModelCard key={model.id} model={model} />
+                ))}
+              </div>
             </div>
           )}
+
+          {/* empty dashboard */}
+          {
+            tabName === 'cart' && (
+              <div className="py-20">
+                <p className="text-4xl text-zinc-400 text-center">Your cart is empty</p>
+              </div>
+            )
+          }
         </div>
       </div>
     </section>
