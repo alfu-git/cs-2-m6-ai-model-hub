feb1c00f50bb3b50dfbfdca549f30c4db69aaa3d
alfu-git <mdalfaz.dev@gmail.com>
2026-03-28 19:48:26 +0600

    display all the AI model on UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Suspense } from 'react'
 import './App.css'
 import Banner from './Components/Banner/Banner'
 import ModelsContainer from './Components/ModelsContainer/ModelsContainer'
 import Navbar from './Components/Navbar/Navbar'
 
+const getModelData = async () => {
+  const fetchModelData = await fetch("../public/models.json");
+  return fetchModelData.json();
+}
+
 function App() {
+const modelDataPromise = getModelData();
 
 
   return (
@@ -11,7 +18,14 @@ function App() {
       
       <Navbar />
       <Banner />
-      <ModelsContainer />
+      
+      <Suspense fallback={(
+        <div className='mt-40 flex justify-center'>
+          <span className="loading loading-spinner text-neutral"></span>
+        </div>
+      )}>
+        <ModelsContainer modelDataPromise={modelDataPromise} />
+      </Suspense>
     </>
   )
 }
--- src/Components/ModelsContainer/ModelsContainer.jsx
+++ src/Components/ModelsContainer/ModelsContainer.jsx
@@ -1,33 +1,57 @@
-import React from 'react';
+import React, { use } from "react";
 import "./ModelsContainer.css";
+import ModelCard from "./ModelCard/ModelCard";
 
-const ModelsContainer = () => {
+const ModelsContainer = ({modelDataPromise}) => {
+
+  const modelData = use(modelDataPromise);
+  console.log(modelData);
+
   return (
-    <section className='mb-20 container mx-auto px-5 border-t border-[gray]/10 rounded-t-xl p-2'>
+    <section className="mb-20 container mx-auto px-5 border-t border-[gray]/10 rounded-t-xl p-2">
       <div>
         <div>
-          {/* tab btn */}
-          <div className='mb-15 flex justify-center'>
-            <div className='space-x-10'>
-              <button className={`
-                  tab-active
-                `}>
-                Models
-              </button>
-              <button className={`
-                  tab-inactive
-                `}>
-                Cart <span>(0)</span>
-              </button>
+          {/* tab and title */}
+          <div>
+            {/* tab btn */}
+            <div className="mb-7 sm:mb-15 flex justify-center">
+              <div className="flex">
+                <button
+                  className={`
+                  btn w-25 sm:w-50 tab-active
+                `}
+                >
+                  Models
+                </button>
+
+                <button
+                  className={`
+                  btn w-25 sm:w-50 tab-inactive
+                `}
+                >
+                  Cart <span>(0)</span>
+                </button>
+              </div>
+            </div>
+
+            {/* title */}
+            <div>
+              <div className="mb-16 text-center">
+                <h2 className="mb-4 text-4xl sm:text-5xl font-bold">
+                  Choose Your AI Model
+                </h2>
+                <p className="text-lg sm:text-xl text-zinc-400">
+                  One subscription gives you access to all frontier AI models
+                </p>
+              </div>
             </div>
           </div>
 
-          {/* title */}
-          <div>
-            <div className='mb-16 text-center'>
-              <h2 className='mb-4 text-5xl font-bold'>Choose Your AI Model</h2>
-              <p className='text-xl text-zinc-400'>One subscription gives you access to all frontier AI models</p>
-            </div>
+          {/* card section */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {
+              modelData.map(model => (<ModelCard key={model.id} model={model} />))
+            }
           </div>
         </div>
       </div>
