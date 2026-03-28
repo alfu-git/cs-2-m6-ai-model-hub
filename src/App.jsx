e91d1985df36119cab65d3b0c37894e53d21a112
alfu-git <mdalfaz.dev@gmail.com>
2026-03-29 01:49:42 +0600

    footer section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,46 @@
-import { Suspense } from 'react'
-  import { ToastContainer, toast } from 'react-toastify';
-import './App.css'
-import Banner from './Components/Banner/Banner'
-import ModelsContainer from './Components/ModelsContainer/ModelsContainer'
-import Navbar from './Components/Navbar/Navbar'
+import { Suspense } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "./App.css";
+import Banner from "./Components/Banner/Banner";
+import ModelsContainer from "./Components/ModelsContainer/ModelsContainer";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 const getModelData = async () => {
   const fetchModelData = await fetch("../public/models.json");
   return fetchModelData.json();
-}
+};
 
 function App() {
-const modelDataPromise = getModelData();
-
+  const modelDataPromise = getModelData();
 
   return (
     <>
-      
-      <Navbar />
-      <Banner />
-      
-      <Suspense fallback={(
-        <div className='mt-40 flex justify-center'>
-          <span className="loading loading-spinner text-neutral"></span>
-        </div>
-      )}>
-        <ModelsContainer modelDataPromise={modelDataPromise} />
-      </Suspense>
+      <header>
+        <Navbar />
+      </header>
 
+      <main>
+        <Banner />
+
+        <Suspense
+          fallback={
+            <div className="mt-40 flex justify-center">
+              <span className="loading loading-spinner text-neutral"></span>
+            </div>
+          }
+        >
+          <ModelsContainer modelDataPromise={modelDataPromise} />
+        </Suspense>
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
 
       <ToastContainer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
