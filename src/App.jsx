2a0ce5ce10d2310f4be85c23d28815b3906d0e70
alfu-git <mdalfaz.dev@gmail.com>
2026-03-28 16:49:17 +0600

    banner done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Banner from './Components/Banner/Banner'
 import Navbar from './Components/Navbar/Navbar'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       
       <Navbar />
+      <Banner />
     </>
   )
 }
